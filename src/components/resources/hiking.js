f14198931d3335cc91c02ebc4a7e0e2d43abbe05
Kat Connolly <hi.connkat@gmail.com>
2022-04-20 14:56:36 -0700

    removed bonus resources and fixed RMMC links

--- src/components/resources/hiking.js
+++ src/components/resources/hiking.js
@@ -1,40 +1,51 @@
 import "../../pages/Resources.css";
 import hiking from "../../images/hiking.png";
 import rmmc from "../../images/rmmc.png";
-// import rmmcLogo from "../../images/RMMCo_Logo.png";
+import rmmcLogo from "../../images/RMMCo_Logo.png";
 
 function Hiking() {
   return (
-    <div className="desktopContent">
-      <div className="section">
-        <div className="title">
-          <img src={hiking} className="title-img" alt="hiking" />
-        </div>
-        <div className="content">
-          <div className="hiking">
-            <a href="http://rockymountainmerit.co">
-              <img src={rmmc} className="nav-links" alt="nav-logo" />
-            </a>
-            <div className="iframe">
-              <iframe
-                width="600"
-                height="420"
-                src="https://www.youtube.com/embed/vH_2PWIVLMg"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
+    <>
+      <div className="desktopContent">
+        <div className="section">
+          <div className="title">
+            <img src={hiking} className="title-img" alt="hiking" />
+          </div>
+          <div className="content">
+            <div className="hiking">
+              <a href="https://www.instagram.com/rockymountainmeritco">
+                <img src={rmmc} className="nav-links" alt="nav-logo" />
+              </a>
+              <div className="iframe">
+                <iframe
+                  width="600"
+                  height="420"
+                  src="https://www.youtube.com/embed/vH_2PWIVLMg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
             </div>
           </div>
-          {/* <div className="mobileContent">
-            <a href="http://rockymountainmerit.co">
-              <img src={rmmcLogo} className="nav-links" alt="nav-logo" />
-            </a>
-          </div> */}
         </div>
       </div>
-    </div>
+      <div className="hikingMobile">
+        <div className="section">
+          <div className="title">
+            <img src={hiking} className="title-img" alt="hiking" />
+          </div>
+          <div className="content">
+            <ul>
+              <a href="https://www.instagram.com/rockymountainmeritco">
+                <img src={rmmcLogo} className="nav-links" alt="nav-logo" />
+              </a>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/resources/writing.js
+++ src/components/resources/writing.js
@@ -1,4 +1,4 @@
-import "../../pages/Resources.css"
+import "../../pages/Resources.css";
 import writing from "../../images/writing.png";
 import medium from "../../images/medium.png";
 
@@ -9,42 +9,11 @@ function Writing() {
         <img src={writing} className="title-img" alt="writing" />
       </div>
       <div className="content">
-        <div className="desktopContent">
-          <ul>
-            <a href="https://www.linkedin.com/pulse/should-you-learn-code-while-self-isolation-resources-kat-connolly">
-              Should You Learn to Code in Self-Isolation
-            </a>
-          </ul>
-        </div>
-        <div className="desktopContent">
-          <ul>
-            <a href="https://www.linkedin.com/posts/connkat_how-to-get-involved-with-calgarys-tech-scene-activity-6749479086315462657-nrdT">
-              How to Get Involved in Calgary's Tech Scene
-            </a>
-          </ul>
-        </div>
         <ul>
-          <b>
-            Check me out on{" "}
-            <a href="https://connkat.medium.com/">
-              <img src={medium} alt="medium" className="medium" />
-            </a>
-          </b>
+          <a href="https://connkat.medium.com/">
+            <img src={medium} alt="medium" className="medium" />
+          </a>
         </ul>
-        <div className="desktopContent">
-          <ul>
-            <a href="https://www.linkedin.com/pulse/practical-reasons-against-bootcamps-kat-connolly/?trackingId=pvQYEK%2B3SsqrhZwyZW2j0w%3D%3D">
-              Practical Reasons For (and Against) Bootcamps
-            </a>
-          </ul>
-        </div>
-        <div className="desktopContent">
-          <ul>
-            <a href="https://www.lighthouselabs.ca/en/blog/how-i-learned-to-love-errors?utm_source=facebook&utm_medium=social&utm_campaign=imposter-syndrome-quote">
-              Imposter Syndrome: How I Learned to Love Errors
-            </a>
-          </ul>
-        </div>
       </div>
     </div>
   );
